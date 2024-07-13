e1a1251cb9b6f724e6fc94c9dbb721020160110a
AlexB <a.basalov@a3f.ru>
2024-07-13 16:25:12 +0400

    jest, jest.config.js

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -1,1 +1,1 @@
-npm run clean && npm run bootstrap && npm run test && npm run prod && npm version update && git push
+npm run clean && npm run bootstrap && npm run test && npm run prod && npm version patch && git push
