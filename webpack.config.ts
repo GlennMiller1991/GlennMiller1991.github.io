0e0b81ee1d93282ee33842467bfd3bda80b28bdf
AlexB <gatesoftommorow91@mail.ru>
2024-08-04 17:06:20 +0400

    webpack.config env types

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,74 +3,81 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const Dotenv = require("dotenv-webpack")
 
 type IEnvConfig = {
-	mode: 'production' | 'development'
+	mode: 'production' | 'development',
+	port: string,
 }
 
-module.exports = (env: IEnvConfig) => ({
-	mode: env.mode,
-	entry: "./src/index.tsx",
-	devtool: "inline-source-map",
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./static/index.html",
-			inject: "head",
-		}),
-		new Dotenv({ path: "./static/.env" }),
-	],
-	resolve: {
-		extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss", ".module.css", ".module.scss"],
-	},
-	output: {
-		filename: "index_[fullhash].js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, "public"),
+module.exports = (env: IEnvConfig) => {
+	env.mode = env.mode || 'development'
+	env.port = env.port || '3000'
+
+	console.log(typeof env.port)
+	return {
+		mode: env.mode,
+		entry: "./src/index.tsx",
+		devtool: "inline-source-map",
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./static/index.html",
+				inject: "head",
+			}),
+			new Dotenv({ path: "./static/.env" }),
+		],
+		resolve: {
+			extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".scss", ".module.css", ".module.scss"],
 		},
-		compress: true,
-		port: 3000,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.s?css$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: "style-loader",
-						options: {
-							injectType: "styleTag",
-						},
-					},
-					{
-						loader: "css-loader",
-						options: {
-							importLoaders: 2,
-							modules: true,
-						},
-					},
-					{
-						loader: "sass-loader",
-					},
-				],
+		output: {
+			filename: "index_[fullhash].js",
+			path: path.resolve(__dirname, "dist"),
+			clean: true,
+		},
+		devServer: {
+			static: {
+				directory: path.join(__dirname, "public"),
 			},
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				loader: "ts-loader",
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]],
+			compress: true,
+			port: env.port,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.s?css$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: "style-loader",
+							options: {
+								injectType: "styleTag",
+							},
+						},
+						{
+							loader: "css-loader",
+							options: {
+								importLoaders: 2,
+								modules: true,
+							},
+						},
+						{
+							loader: "sass-loader",
+						},
+					],
+				},
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					loader: "ts-loader",
+				},
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]],
+						},
 					},
 				},
-			},
-		],
-	},
-})
+			],
+		},
+	}
+}
